5a42059a643cd0d11e56bcd1fc944822d5f44e0d
Subhashree Satpathy <satpathysubhashree2109@gmail.com>
2025-12-31 15:59:43 +0530

    Added new file

---
+++ .vscode/swapnodesinPairs.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+    if (!head || !head->next) return head;
+
+    struct ListNode* newHead = head->next;
+    head->next = swapPairs(newHead->next);
+    newHead->next = head;
+    return newHead;
+}
+    
+};
