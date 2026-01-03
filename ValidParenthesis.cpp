bbca61470d82b5de9e441b276a6903b4ada607d1
Subhashree Satpathy <satpathysubhashree2109@gmail.com>
2026-01-03 10:59:54 +0530

    Added new file

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            // Push opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            // Handle closing brackets
+            else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // Stack should be empty at the end
+        return st.empty();
+    }
+};
